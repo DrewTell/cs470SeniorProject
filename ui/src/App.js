a3dcc8b82ed1f86764035953f3d21e4a76024cff
Alejandro M <74997012+AMR-1015@users.noreply.github.com>
2022-10-28 16:33:42 -0700

    Add files via upload

---
+++ ui/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import Gamecard from "./Components/Gamecard";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Pick Your Game</h1>
+      <Grid container spacing={3}>
+        <Grid sx={{ ml: 8 }} item xs>
+          <Gamecard />
+        </Grid>
+        <Grid item xs>
+          <Gamecard/>
+        </Grid>
+        <Grid item xs>
+          <Gamecard/>
+        </Grid>
+      </Grid>
+      <Button sx={{ mt: 10 }} color="success" variant="contained">Login</Button>
+    </div>
+  );
+}
+
+export default App;
