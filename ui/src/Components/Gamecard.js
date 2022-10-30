76af0c6b794d58bcc9b2cf628187c2f0c8f7b946
Alejandro M <74997012+AMR-1015@users.noreply.github.com>
2022-10-30 13:16:07 -0700

    Added pentago route change
    Once "Play" is clicked the menu routes to pentago

--- ui/src/Components/Gamecard.js
+++ ui/src/Components/Gamecard.js
@@ -5,23 +5,34 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+// import Menu from './Menu';
+import { useNavigate } from 'react-router-dom';
 
-export default function GameCard() {
+
+export default function GameCard(props) {
+  
+
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `Pentago`; 
+    navigate(path);
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
          <CardContent>
         <Typography gutterBottom variant="h4" component="div">
-          Game 1
+          {props.title}
         </Typography>
       </CardContent>
       <CardMedia
         component="img"
         alt="Pentago"
-        height="140"
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJcrIqf1mcUrdlPkh61CZDod3VkTBh3a5mzA&usqp=CAU"
+        height="160"
+        image="https://ae01.alicdn.com/kf/HTB1I49yaRSD3KVjSZFqq6A4bpXaI.jpg"
       />
       <CardActions>
-        <Button size="small">Play</Button>
+        <Button onClick={routeChange} size="small">Play</Button>
         <Button size="small">How to Play</Button>
       </CardActions>
     </Card>
