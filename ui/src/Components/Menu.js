2d096fb098cefc149cf73271639135770084628c
Alejandro M <74997012+AMR-1015@users.noreply.github.com>
2022-10-30 13:16:54 -0700

    Update Menu.js

--- ui/src/Components/Menu.js
+++ ui/src/Components/Menu.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import Gamecard from "./Gamecard";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 
-function Menu() {
-    
-  return (
-    <div>Menu</div>
-  )
-}
-
-export default Menu
+export default function Menu(props) {
+    return (
+      <div className="Menu">
+        <h1 className="title">Game Menu</h1>
+        <Grid container spacing={3}>
+          <Grid sx={{ ml: 8 }} item xs>
+            <Gamecard title='Pentago'/>
+          </Grid>
+          <Grid item xs>
+            <Gamecard title='Game 2'/>
+          </Grid>
+          <Grid item xs>
+            <Gamecard title='Game 3'/>
+          </Grid>
+        </Grid>
+        <Button sx={{ mt: 10 }} color="success" variant="contained">
+          Login
+        </Button>
+      </div>
+    );
+  }
