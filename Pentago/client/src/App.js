1be2334ddf31e88ce3420e77348ae1778df52570
DrewTell <98501727+DrewTell@users.noreply.github.com>
2022-11-07 12:18:10 -0800

    Added Appbar and sign in toggle
    Sticky Appbar with a button to navigate further profile/account/ or chose to log out. A toggle button between sign in and login

--- Pentago/client/src/App.js
+++ Pentago/client/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import ToggleButtonNotEmpty from "./components/userSignInToggle";
+import Button from '@mui/material/Button';
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import { useState } from "react";
 import JoinPentago from "./components/pentago/JoinPentago";
+import PlaceHolderGame from "./components/PlaceHolder";
+import React, {Fragment} from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Grid from "@mui/material/Grid";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {CssBaseline, Divider} from "@mui/material";
+import ResponsiveAppBar from "./components/AppBar";
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+
 function App() {
   const api_key = "ujt9j62cn6xx";
   const cookies = new Cookies();
@@ -42,22 +59,47 @@ function App() {
         });
   }
   return (
-      <div className="App">
+      <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+      <Fragment>
+
+          <ResponsiveAppBar/>
         {isAuth ? (
-            <Chat client={client}>
+            <Stack direction="column" divider={<Divider orientation="horizontal" flexItem />} sx = {{xs: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Box  sx={{  width: '50%' }}>
+                <Chat client={client}>
                 <JoinPentago />
 
-              <button onClick={logOut}> Log Out</button>
+
 
             </Chat>
-
+            </Box>
+                <Box sx = {{width: '50%'}}>
+                    <PlaceHolderGame/>
+                </Box>
+                </Stack>
         ) : (
-            <>
-              <SignUp setIsAuth={setIsAuth} />
-              <Login setIsAuth={setIsAuth} />
-            </>
-        )}
-      </div>
+
+                <Box component="span" sx={{ display: 'block', p: 35, border: '1'}}>
+                <ToggleButtonNotEmpty setIsAuth={setIsAuth} />
+                </Box>
+
+        )
+        }
+          <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              style={{ minHeight: '100vh' }}
+          >
+
+              <Grid item xs={3}>
+                  <Button onClick={logOut} sx={{xs: 'flex', alignItems: 'center', justifyContent: 'center' }}> Log Out</Button>
+              </Grid>
+          </Grid>
+      </Fragment>
+      </ThemeProvider>
   );
 }
 
