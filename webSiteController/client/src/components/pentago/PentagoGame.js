62f3c95682b24e274ddd684d8a4ab301358c9107
DrewTell <98501727+DrewTell@users.noreply.github.com>
2022-11-21 12:07:40 -0800

    Pentago loading screen

--- webSiteController/client/src/components/pentago/PentagoGame.js
+++ webSiteController/client/src/components/pentago/PentagoGame.js
@@ -4,9 +4,13 @@ import Board from "./PentagoBoard";
 import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
 import "./Chat.css";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 
 import { Window, MessageList, MessageInput } from "stream-chat-react";
+import Grid from "@mui/material/Grid";
 function Game({ channel, setChannel, setGameSelected }) {
     const [playersJoined, setPlayersJoined] = useState(
         channel.state.watcher_count === 2
@@ -17,7 +21,16 @@ function Game({ channel, setChannel, setGameSelected }) {
         setPlayersJoined(event.watcher_count === 2);
     });
     if (!playersJoined) {
-        return <div> Waiting for other player to join...</div>;
+        return <Fragment>
+            <Grid align="center" sx={{justifyContent: 'center', mt: 4, mb: 4, ml: 18}}>
+            <Stack direction="row">
+            <Typography variant='h4' textAlign='center'> Waiting for the other player to join...  </Typography>
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress />
+            </Box>
+            </Stack>
+            </Grid>
+        </Fragment>
     }
 
     return (
