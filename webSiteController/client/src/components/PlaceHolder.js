031a67a9f5d4da9e1dbe9156ad9e577de92e5ec3
DrewTell <98501727+DrewTell@users.noreply.github.com>
2022-11-21 08:28:14 -0800

    can Click Placeholder
    Placeholder has bene implemented as a template for adding single player games.

--- webSiteController/client/src/components/PlaceHolder.js
+++ webSiteController/client/src/components/PlaceHolder.js
@@ -1,36 +1,17 @@
 import React, { useState } from "react";
-import { useChatContext, Channel } from "stream-chat-react";
-import CustomInput from "./CustomInput";
+import Button from "@mui/material/Button";
+import Grid from '@mui/material/Grid';
 
-import {TextField} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 
 function PlaceHolderGame(props) {
-    const [rivalUsername, setRivalUsername] = useState("");
-    const { client } = useChatContext();
-    const [channel, setChannel] = useState(null);
-    const createChannel = async () => {
-        const response = await client.queryUsers({ name: { $eq: rivalUsername } });
-
-        if (response.users.length === 0) {
-            alert("User not found");
-            return;
-        }
-
-        const newChannel = await client.channel("messaging", {
-            members: [client.userID, response.users[0].id],
-        });
-
-        await newChannel.watch();
-        setChannel(newChannel);
-    };
     return (
         <>
-            {channel ? (
-                <Channel channel={channel} Input={CustomInput}>
-                    <div> Work in Progress </div>
-                </Channel>
+            {props.isSelected === 'placeHolder' ? (
+                    <Grid sx={{mt: 2, justifyContent: 'center', display: { xs: 'none', md: 'flex' }}}>
+                        <Button onClick={() => {props.setGameSelected(false); props.setIsSelected(null)}}> Leave Game </Button>
+                    </Grid>
             ) : props.gameSelected === false ? (
                 <div className="joinGame">
                     <Typography
@@ -71,13 +52,7 @@ function PlaceHolderGame(props) {
                         This game reimplements the well known ? with a whole new spin: After ?, the player has to make a move, thus
                         changing the game state.
                     </Typography>
-                    <TextField
-                        placeholder="Username of placeholder rival..."
-                        onChange={(event) => {
-                            setRivalUsername(event.target.value);
-                        }}
-                    />
-                    <button onClick={createChannel}> Join/Start Game</button>
+                    <Button onClick={() => {props.setIsSelected('placeHolder'); props.setGameSelected(true)}}> Start Game</Button>
                 </div>
             ) : null }
         </>
