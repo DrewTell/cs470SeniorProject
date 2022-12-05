72d8dd28d08f6eb9b9156794b4d0731d067855d9
Alejandro M <74997012+AMR-1015@users.noreply.github.com>
2022-12-05 10:11:31 -0800

    Update WinningMessage.js

--- webSiteController/client/src/components/Loteria/components/WinningMessage.js
+++ webSiteController/client/src/components/Loteria/components/WinningMessage.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import { Typography, Grid } from "@mui/material";
+import React from "react";
+import { Typography, Grid, Stack, Button } from "@mui/material";
 
 function WinningMessage(props) {
-
   return (
-    <Grid container sx={{ mb:2.5, mx:"auto", justifyContent: "center",}}>
-      <Typography textAlign={"center"} variant="h5"  fontFamily={'Raleway'} color={"white"} gutterBottom>
+    <Stack container sx={{ mb: 2.5, mx: "auto", justifyContent: "center" }}>
+      <Typography
+        textAlign={"center"}
+        variant="h5"
+        fontFamily={"Raleway"}
+        color={"white"}
+        gutterBottom
+      >
         Player {props.player} called Loteria.
       </Typography>
-    </Grid>
-  )
+      <Button
+        sx={{
+          mx: "auto",
+          width: 150,
+          justifyContent: "center",
+        }}
+      >
+        Play Again
+      </Button>
+    </Stack>
+  );
 }
 
-export default WinningMessage
+export default WinningMessage;
